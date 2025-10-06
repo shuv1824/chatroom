037568cb26f9e7863d0bfd1ae4625ba7756451e8
Shah Nawaz <shuvo1824@hotmail.com>
2025-10-06 21:16:09 +0600

    chore: form and htmx added to chat ui

--- a_rtchat/views.py
+++ a_rtchat/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.contrib.auth.decorators import login_required
 
+from a_rtchat.forms import ChatMessageCreateForm
 from a_rtchat.models import ChatGroup
 
 
@@ -8,4 +9,21 @@ from a_rtchat.models import ChatGroup
 def chat_view(request):
     chat_group = get_object_or_404(ChatGroup, group_name="public-chat")
     chat_messages = chat_group.chat_messages.all()[:30]
-    return render(request, "a_rtchat/chat.html", {"chat_messages": chat_messages})
+    form = ChatMessageCreateForm()
+
+    if request.method == "POST":
+        form = ChatMessageCreateForm(request.POST)
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.author = request.user
+            message.group = chat_group
+            message.save()
+            context = {
+                "message": message,
+                "user": request.user,
+            }
+            return render(request, "a_rtchat/partials/chat_message_p.html", context)
+
+    return render(
+        request, "a_rtchat/chat.html", {"chat_messages": chat_messages, "form": form}
+    )
